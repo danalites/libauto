fea167942e9cc83f19f3248c5aa864068697b322
vrtee <brooks.li1998@gmail.com>
2023-03-10 20:03:08 -0500

    chore: remove legacy docker image

--- docker/duckling/test.sh
+++
@@ -1,5 +0,0 @@
-curl -XPOST http://localhost:8868/parse --data 'locale=en_US&text="My number is 4111-1111-1111-1111. card"&dims="["credit-card-number"]"'
-
-echo "\n-----\n"
-
-curl -XPOST http://localhost:8868/parse --data 'locale=en_US&text="7932742"&dims="["Ordinal"]"'
--- docker/uiRec/test.sh
+++
@@ -1,1 +0,0 @@
-python server.py --img image4.png --op_dir ./ --min_grad 5 --ffl_block 5 --min_ele_area 50 --max_word_inline_gap 4 --max_line_gap 4 --merge_contained_ele
