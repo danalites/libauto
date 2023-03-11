8ce58d20c21ca6313877fdd6aca757eb1607be83
vrtee <brooks.li1998@gmail.com>
2023-03-10 20:04:00 -0500

    chore: remove legacy tests

--- tests/test_draw_screen.py
+++
@@ -1,1 +0,0 @@
-# https://stackoverflow.com/a/7585663
--- tests/test_web_ops.py
+++
@@ -1,9 +0,0 @@
-from libauto import run
-
-
-def test_monitor():
-    run("bestbuy-restock-monitor.yaml")
-
-
-if __name__ == "__main__":
-    test_monitor()
